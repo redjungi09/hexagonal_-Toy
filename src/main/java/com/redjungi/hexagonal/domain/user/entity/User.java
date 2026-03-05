3047f11bda184d981c4d9c1d8988045658efc256
redjungi <redjungi09@gmail.com>
2026-03-05 21:12:07 +0900

    feat: User엔티티 작성

--- src/main/java/com/redjungi/hexagonal/domain/user/entity/User.java
+++ src/main/java/com/redjungi/hexagonal/domain/user/entity/User.java
@@ -1,4 +1,18 @@
 package com.redjungi.hexagonal.domain.user.entity;
 
-public class User {
+
+public record User(
+        Long id,
+        String username,
+        String password,
+        String email
+) {
+    public   static User create(String username, String password, String email){
+        return new User(
+          null,
+          username,
+          password,
+          email
+        );
+    }
 }

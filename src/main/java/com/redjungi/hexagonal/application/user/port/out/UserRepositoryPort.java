1513b468ad43785d57718ea6b5db7d59cfc35e47
redjungi <redjungi09@gmail.com>
2026-03-07 20:15:22 +0900

    refactor: Component삭제

--- src/main/java/com/redjungi/hexagonal/application/user/port/out/UserRepositoryPort.java
+++ src/main/java/com/redjungi/hexagonal/application/user/port/out/UserRepositoryPort.java
@@ -1,9 +1,7 @@
 package com.redjungi.hexagonal.application.user.port.out;
 
 import com.redjungi.hexagonal.domain.user.entity.User;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface UserRepositoryPort {
     User save(User user);
 
